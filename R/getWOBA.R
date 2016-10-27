fba7b5c8f3d0497a1c91ba54e09d1d1fb3b1c252
Keberwein <kristian.eberwein@yahoo.com>
2016-10-27 16:22:56 -0400

    update woba

--- R/getWOBA.R
+++ R/getWOBA.R
@@ -39,6 +39,13 @@ RunValues <- subset(LeagueRunsPerOut, select=c("yearID", "RperOut")) %>%
 # Use Batting table to find the runsPlus and runsMinus values to use in the wOBA multiplier.
 batting <- Lahman::Batting
 
+yearbatting <- subset(batting, select=c("yearID", "AB", "R", "H", "X2B", "X3B", "HR",
+                                        "SB", "CS", "BB", "SO", "IBB", "HBP", "SF")) %>%
+    group_by(yearID) %>%
+    summarise(AB=sum(AB), R=sum(R), H=sum(H), X2B=sum(X2B), X3B=sum(X3B), HR=sum(HR),
+              SB=sum(SB), CS=sum(CS), BB=sum(BB), SO=sum(SO), IBB=sum(IBB), HBP=sum(HBP),
+              SF=sum(SF))
+
 runsBatting <- left_join(batting, RunValues, by="yearID")
 
 
