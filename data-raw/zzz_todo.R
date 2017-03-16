c80433c087127aa88e16735998b7c878dacee120
Keberwein <kristian.eberwein@yahoo.com>
2017-03-16 15:03:12 -0400

    update todos

--- data-raw/zzz_todo.R
+++ data-raw/zzz_todo.R
@@ -8,7 +8,7 @@
 
 # Think of new package name.
 
-# Add / write vignettes.
+# Add / write vignettes. use drob's post http://varianceexplained.org/r/empirical_bayes_baseball/ Maybe some of those as functions.
 
 # Expand README with database example.
 
