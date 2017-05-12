b92aee69defb3e467c93306afb590dd3453e96c5
Keberwein <kristian.eberwein@yahoo.com>
2017-05-12 14:56:44 -0400

    edit REAMDE, add NEWS

---
+++ NEWS.md
@@ -0,0 +1,7 @@
+# baseballDBR 0.1.0
+
+## New features
+
+* Added vignettes
+
+* Added `get_bbdb()` function.
--- data-raw/zzz_todo.R
+++ data-raw/zzz_todo.R
@@ -1,12 +1,6 @@
 # CRAN TODO LIST:
 
-# Figure out how to load multiple tables at once using dbWritetable().
-
 # Add / write vignettes. use drob's post http://varianceexplained.org/r/empirical_bayes_baseball/ Maybe some of those as functions.
-
-# Write Lahman crosswalk functin to rename columns. Dplyr rename will be good for this.
-
-# Add notes MD
 
 # Check the FIP function one last time.
 
