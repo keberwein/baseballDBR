394f12b1f2392af3c3cdbd8becfcb723313e1535
Keberwein <kristian.eberwein@yahoo.com>
2017-06-02 18:02:59 -0400

    updated todo list

--- data-raw/zzz_todo.R
+++ data-raw/zzz_todo.R
@@ -2,3 +2,11 @@
 
 # Add / write vignettes. use drob's post http://varianceexplained.org/r/empirical_bayes_baseball/ Maybe some of those as functions.
 
+# Add to woba vin.
+
+# create wRAA/wRC vin
+
+# create fips vin
+
+# create database creation vin with postgres and mysql
+
